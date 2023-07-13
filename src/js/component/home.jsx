6d92fc921d1405422f4b930af6436091008f0830
Ana Plata <acplata@Anas-Air.lan>
2023-07-12 21:45:29 -0400

    intial

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,26 @@
-import React from "react";
-
+import React, { useState } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [active, setActive] = useState("")
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container vh-100 p-5">
+			<div className="top bg-black m-auto d-flex flex-column align-items-center justify-content-center"></div>
+			<div className="box w-25 h-50 bg-black m-auto d-flex flex-column align-items-center justify-content-center">
+				<div className={`light red ${active === "red" && "active"}`}
+					onClick={() => setActive("red")}
+				></div>
+				<div className={`light yellow ${active === "yellow" && "active"}`}
+					onClick={() => setActive("yellow")}
+				></div>
+				<div className={`light green ${active === "green" && "active"}`}
+					onClick={() => setActive("green")}
+				></div>
+			</div>
+
 		</div>
 	);
 };
